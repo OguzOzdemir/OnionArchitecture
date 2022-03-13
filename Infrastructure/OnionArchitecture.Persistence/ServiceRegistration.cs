ac82b7a78caa06e04938b5abad40dd6e45f3e93d
oguzozdemir <oguzozdemir_89@hotmail.com>
2022-03-13 18:12:52 +0300

    Add project files.

---
+++ Infrastructure/OnionArchitecture.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionArchitecture.Application.Abstractions;
+using OnionArchitecture.Persistence.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}

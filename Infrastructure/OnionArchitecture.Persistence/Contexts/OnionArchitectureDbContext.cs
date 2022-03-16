c9585a46f670010c41987ece02531dde5fe41716
oguzozdemir <oguzozdemir_89@hotmail.com>
2022-03-16 23:16:17 +0300

    add migrations + db context

---
+++ Infrastructure/OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence.Contexts
+{
+    public class OnionArchitectureDbContext : DbContext
+    {
+        public OnionArchitectureDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
--- Infrastructure/OnionArchitecture.Persistence/ServiceRegistration.cs
+++ Infrastructure/OnionArchitecture.Persistence/ServiceRegistration.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using OnionArchitecture.Application.Abstractions;
-using OnionArchitecture.Persistence.Concretes;
+using OnionArchitecture.Persistence.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnionArchitecture.Persistence
 {
@@ -13,7 +13,7 @@ namespace OnionArchitecture.Persistence
     {
         public static void AddPersistanceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<OnionArchitectureDbContext>(options => options.UseSqlServer(@"Data Source=.;Initial Catalog=OnionArchitectureDb;Integrated Security=False;Persist Security Info=False;User ID=sa;Password=1234"));
         }
     }
 }
